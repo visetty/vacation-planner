3bda9b4400610b8d2a2012ed8e7bc59bf0d9e2e9
visetty <visalalakkimsetty@gmail.com>
2018-06-09 15:44:57 -0700

    adds user interface logic to call backend functions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,50 @@ var displaySuggestion = function(placeId) {
   $('#btn-tryagain').show();
   $('#vacationideas').hide();
 }
+
+//user interface logic
+
+$(document).ready(function() {
+   $("form#vacationideas").submit(function(event) {
+    event.preventDefault();
+    var vacationtype = parseInt($("#vacationtype").val());
+    //alert(vacationtype);
+    var placetogo = parseInt($("#placetogo").val());
+    //alert(placetogo);
+    var weathertype = parseInt($("#weathertype").val());
+    //alert(weathertype);
+    var daystogo = parseInt($("#daystogo").val());
+    //alert(daystogo);
+    var budget = parseInt($("#budget").val());
+    //alert(budget);
+    var result = vacationSuggestion(vacationtype, placetogo, weathertype, daystogo, budget);
+
+    hideALLOptions();
+    if (result>=5 && result<10) {
+      //$('#vacation1').show();
+      displaySuggestion('#vacation1');
+    }
+    else if (result>10 && result<=15) {
+      //$('#vacation2').show();
+      displaySuggestion('#vacation2');
+    }
+    else if (result>15 && result<18) {
+      //$('#vacation3').show();
+      displaySuggestion('#vacation3');
+    }
+    else if (result<=20){
+      //$('#vacation4').show();
+      displaySuggestion('#vacation4');
+    }
+    else {
+      alert("Please make proper selection");
+    }
+  });
+
+$("#btn-tryagain").click(function() {
+  document.getElementById("vacationideas").reset();
+  $('#vacationideas').show();
+  $('#btn-tryagain').hide();
+  hideALLOptions();
+});
+});
